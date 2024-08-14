3d1336830b5afe9d88e072bb89fcc323720ce891
= <=>
2024-08-13 20:06:07 -0400

    Update image at 1 A.M
    Script ran everyday at midnight, never leaving enought time for the
    image to update server side. Commit fixes issue, by changing the time
    for the script to execute at 1 a.m.

--- get_image.sh
+++ get_image.sh
@@ -21,7 +21,8 @@ function install () {
 	cat - > update_image_crontab <<delim
 # Following will record current directory in environment variable.
 IOTD=$PWD
-@daily $PWD/get_image.sh -u
+# Run script every first hour of everyday.
+0 1 * * *	$PWD/get_image.sh -u
 delim
 # User must be on `cron.allow` list; or must not be on `cron.deny`; otherwise should run as super-user.
 crontab -u $USER $PWD/update_image_crontab
