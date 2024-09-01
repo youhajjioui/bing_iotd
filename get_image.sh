6a08dc3eb291405a4d36c750a499f1bfa5315004
Delusive Miraj <delusivemiraj@outlook.com>
2024-09-01 17:21:31 -0400

    Well script should execute at 7 A.M
    The picture updates everyday at 7 a.m eastern time.

--- get_image.sh
+++ get_image.sh
@@ -22,7 +22,7 @@ function install () {
 # Following will record current directory in environment variable.
 IOTD=$PWD
 # Run script every first hour of everyday.
-0 1 * * *	$PWD/get_image.sh -u
+0 7 * * *	$PWD/get_image.sh -u
 delim
 # User must be on `cron.allow` list; or must not be on `cron.deny`; otherwise should run as super-user.
 crontab -u $USER $PWD/update_image_crontab
